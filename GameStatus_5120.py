c7fc48ec51c9788e880f98fb659b9aa8cda3431a
Melissa Castro <castromelissa2004@gmail.com>
2024-10-13 12:40:52 -0700

    Finished the Game status file! functions need to be checked/reviewed still

--- GameStatus_5120.py
+++ GameStatus_5120.py
@@ -36,6 +36,10 @@ class GameStatus:
 		
 
 	def get_scores(self, terminal):
+		'''
+		******************************************Check this one*****************************
+		did we use the cols/range right??? what do i do with the check_point variable???
+		'''
 		"""
 		YOUR CODE HERE TO CALCULATE THE SCORES. MAKE SURE YOU ADD THE SCORE FOR EACH PLAYER BY CHECKING 
 		EACH TRIPLET IN THE BOARD IN EACH DIRECTION (HORIZONAL, VERTICAL, AND ANY DIAGONAL DIRECTION)
@@ -47,6 +51,56 @@ class GameStatus:
 		cols = len(self.board_state[0])
 		scores = 0
 		check_point = 3 if terminal else 2
+		# we will have to check the rows, columns, and diagonals
+
+		# lets check rows first
+		for rows in range(rows):
+			# check rows
+			# we have to have the -2 to make sure we dont go ou of bounds
+			for col in range(cols-check_point +1):
+				# the if statement below says if 0,0 0,1 or 0,3 are NOT 0 and then seeing 
+				# if they are either a row of 1 or 2 (0 or X) then it is a point
+				if (all(self.board_state[row,col+i] == self.board_state[row][col])):
+					# if that is true lets check if it is a X or a 0 (remember 0 is 1 and 2 is X)
+					if (self.board_state[row][col] == 2):
+						# if there is a row of X, player 2 or AI wins
+						score -= 1
+					else:
+						# if there is a row of 0, player 1 wins
+						score += 1
+		
+		# now let's check the columns
+		for col in range(cols):
+			for row in range(rows-check_point):
+				if (all(self.board_state[row + i,col] == self.board_state[row][col] !=0 for i in range(check_point))):
+				# if that is true lets check if it is a X or a 0 (remember 0 is 1 and 2 is X)
+					if (self.board_state[row][col] == 2):
+						# if there is a row of X, player 2 or AI wins
+						score -= 1
+					else:
+						# if there is a row of 1, player 1 wins
+						score += 1
+		'''
+		These might definitely be wrong, i didnt know how to use checkpoint here??
+		'''
+		# now we check diagonal of 11 22 33 or 00 11 22 aka the \ and next we check the / diagonal
+		if (self.board_state[0][0] == self.board_state[1][1] == self.board_state[2][2] !=0):
+			# we see what values they have
+			if self.board_state[0][0] == 1:
+				score += 1
+			else:
+				score -=1
+		
+		# now we check the other diagonal
+		if (self.board_state[0][2] == self.board_state[1][1] == self.board_state[2][0] !=0):
+			# we see what values they have
+			if self.board_state[0][2] == 1:
+				score += 1
+			else:
+				score -=1
+		# finally, we return final score
+		return score
+
 		
 	    
 
@@ -69,9 +123,23 @@ class GameStatus:
         YOUR CODE HERE TO ADD ALL THE NON EMPTY CELLS TO MOVES VARIABLES AND RETURN IT TO BE USE BY YOUR
         MINIMAX OR NEGAMAX FUNCTIONS
         """
+		# we will ahve to iterate through rows and columns to count number of empty cells
+		# we need to check if there are still 0's on the board aka new moves to be found
+		if (self.game_state == 0):
+			# we use the same logic as in egt scores to check if there are 0s on board
+			rows = len(self.board_state)
+			cols = len(self.board_state[0])
+
+			# now check the rows and columns for mvoes
+			for row in range(rows):
+				for col in range(cols):
+					# of this specific board place is a 0, save the board space
+					if (self.board_state[row][col] == 0):
+						moves.append((row,col))
+		#return list of moves
 		return moves
 
-	# 
+	# gets new state for board
 	def get_new_state(self, move):
 		new_board_state = self.board_state.copy()
 		x, y = move[0], move[1]

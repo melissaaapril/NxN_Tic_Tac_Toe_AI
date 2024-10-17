b89760c834176bb2e758e4b72ad2971f2baf6354
ceo of swagger <93799269+zhadyz@users.noreply.github.com>
2024-10-16 23:59:33 -0700

    Add files via upload

--- GameStatus_5120.py
+++ GameStatus_5120.py
@@ -1,71 +1,85 @@
-# -*- coding: utf-8 -*-
-
+# GameStatus_5120.py
+import numpy as np
 
 class GameStatus:
+    def __init__(self, board_state, player_turn):
+        self.board_state = board_state  # game board matrix
+        self.player_turn = player_turn  # 1 for human, -1 for AI
+        self.winner = ""  # track winner
 
+    def is_terminal(self):
+        # check if there are any empty cells. if none, game over
+        if np.any(self.board_state == 0):
+            return False  # game is ongoing
+        else:
+            # determine winner if no empty cells
+            score = self.get_scores(terminal=True)
+            if score > 0:
+                self.winner = "Human"
+            elif score < 0:
+                self.winner = "AI"
+            else:
+                self.winner = "Draw"
+            return True  # game is over
 
-	def __init__(self, board_state, turn_O):
+    def get_scores(self, terminal):
+        rows = self.board_state.shape[0]
+        cols = self.board_state.shape[1]
+        scores = 0
 
-		self.board_state = board_state
-		self.turn_O = turn_O
-		self.oldScores = 0
+        # check rows for any triplets of 1s or -1s
+        for i in range(rows):
+            for j in range(cols - 2):
+                triplet = self.board_state[i, j:j+3]
+                if np.all(triplet == 1):
+                    scores += 1
+                elif np.all(triplet == -1):
+                    scores -= 1
 
-		self.winner = ""
+        # check columns for triplets
+        for i in range(rows - 2):
+            for j in range(cols):
+                triplet = self.board_state[i:i+3, j]
+                if np.all(triplet == 1):
+                    scores += 1
+                elif np.all(triplet == -1):
+                    scores -= 1
 
-	# 
-	def is_terminal(self):
-		"""
-        YOUR CODE HERE TO CHECK IF ANY CELL IS EMPTY WITH THE VALUE 0. IF THERE IS NO EMPTY
-        THEN YOU SHOULD ALSO RETURN THE WINNER OF THE GAME BY CHECKING THE SCORES FOR EACH PLAYER 
-        """
-		
+        # check diagonals for triplets
+        for i in range(rows - 2):
+            for j in range(cols - 2):
+                # main diagonal
+                triplet = [self.board_state[i+k, j+k] for k in range(3)]
+                if all(x == 1 for x in triplet):
+                    scores += 1
+                elif all(x == -1 for x in triplet):
+                    scores -= 1
 
-	def get_scores(self, terminal):
-		"""
-        YOUR CODE HERE TO CALCULATE THE SCORES. MAKE SURE YOU ADD THE SCORE FOR EACH PLAYER BY CHECKING 
-        EACH TRIPLET IN THE BOARD IN EACH DIRECTION (HORIZONAL, VERTICAL, AND ANY DIAGONAL DIRECTION)
-        
-        YOU SHOULD THEN RETURN THE CALCULATED SCORE WHICH CAN BE POSITIVE (HUMAN PLAYER WINS),
-        NEGATIVE (AI PLAYER WINS), OR 0 (DRAW)
-        
-        """        
-		rows = len(self.board_state)
-		cols = len(self.board_state[0])
-		scores = 0
-		check_point = 3 if terminal else 2
-		
-	    
+                # anti-diagonal
+                triplet = [self.board_state[i+2-k, j+k] for k in range(3)]
+                if all(x == 1 for x in triplet):
+                    scores += 1
+                elif all(x == -1 for x in triplet):
+                    scores -= 1
 
-	def get_negamax_scores(self, terminal):
-		"""
-        YOUR CODE HERE TO CALCULATE NEGAMAX SCORES. THIS FUNCTION SHOULD EXACTLY BE THE SAME OF GET_SCORES UNLESS
-        YOU SET THE SCORE FOR NEGAMX TO A VALUE THAT IS NOT AN INCREMENT OF 1 (E.G., YOU CAN DO SCORES = SCORES + 100 
-                                                                               FOR HUMAN PLAYER INSTEAD OF 
-                                                                               SCORES = SCORES + 1)
-        """
-		rows = len(self.board_state)
-		cols = len(self.board_state[0])
-		scores = 0
-		check_point = 3 if terminal else 2
-	    
+        return scores
 
-	def get_moves(self):
-		moves = []
-		"""
-        YOUR CODE HERE TO ADD ALL THE NON EMPTY CELLS TO MOVES VARIABLES AND RETURN IT TO BE USE BY YOUR
-        MINIMAX OR NEGAMAX FUNCTIONS
-        """
-		return moves
+    def get_negamax_scores(self, terminal):
+        # negamax uses same scoring function
+        return self.get_scores(terminal)
 
-	# 
-	def get_new_state(self, move):
-		new_board_state = self.board_state.copy()
-		x, y = move[0], move[1]
-		new_board_state[x,y] = 1 if self.turn_O else -1
-		return GameStatus(new_board_state, not self.turn_O)
-'''
-Resources below:
+    def get_moves(self):
+        moves = []
+        rows, cols = self.board_state.shape
+        # check each cell for possible moves
+        for i in range(rows):
+            for j in range(cols):
+                if self.board_state[i, j] == 0:
+                    moves.append((i, j))  # add empty cell coordinates
+        return moves
 
-https://www.youtube.com/watch?v=LbTu0rwikwg
-
-'''
+    def get_new_state(self, move):
+        new_board_state = self.board_state.copy()  # create copy of board
+        x, y = move[0], move[1]
+        new_board_state[x, y] = self.player_turn  # apply move
+        return GameStatus(new_board_state, -self.player_turn)  # switch player turn
